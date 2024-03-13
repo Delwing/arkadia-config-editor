83d9574ffc80c7728a209c0b926645a3c233d610
Piotr Wilczynski <delwing@gmail.com>
2024-03-13 14:58:44 +0100

    update building process and missing settings issue

--- src/main/handlers/theme-handlers.ts
+++ src/main/handlers/theme-handlers.ts
@@ -1,6 +1,6 @@
-import settings from "electron-settings";
-import {ipcMain} from "electron";
+import settings from 'electron-settings'
+import { ipcMain } from 'electron'
 
 ipcMain.handle('theme:bootstrap', () => {
-  return settings.getSync('theme.bootstrap')
+  return settings.getSync('theme.bootstrap') ?? 'sandstone'
 })
