47d4d00e41be51e12008770bf780e5bd226b3015
Piotr Wilczynski <delwing@gmail.com>
2024-03-30 16:14:38 +0100

    fix checkbox save

--- src/renderer/src/editor/components/CheckBoxInput.tsx
+++ src/renderer/src/editor/components/CheckBoxInput.tsx
@@ -1,15 +1,15 @@
-import { Container, FormCheck } from 'react-bootstrap'
-import { JSX } from 'react'
-import { InputProperties } from '../Components'
+import {Container, FormCheck} from 'react-bootstrap'
+import {JSX} from 'react'
+import {InputProperties} from '../Components'
 
 export function CheckBoxInput(options: string[]) {
-  return function CheckBoxImplementation({ name, value, updateCallback }: InputProperties): JSX.Element {
-    const current = value as (number | string)[]
+  return function CheckBoxImplementation({name, value, updateCallback}: InputProperties): JSX.Element {
+    const current = value as string[]
 
     return (
       <>
         <Container fluid={true} className={'form-control'}>
-          {options.map((label: string | number) => (
+          {options.map((label : string) => (
             <FormCheck
               key={label}
               id={`${name}.${label}`}
@@ -18,9 +18,7 @@ export function CheckBoxInput(options: string[]) {
               value={label}
               onChange={(e) =>
                 updateCallback(
-                  JSON.stringify(
-                    e.currentTarget.checked ? current.concat([label]) : current.filter((current) => current !== label)
-                  )
+                  e.currentTarget.checked ? current.concat([label]) : current.filter((current) => current !== label)
                 )
               }
             />
