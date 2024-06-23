4cbe623a7968e324ccfb8df6cecbcdc627db1453
Piotr Wilczynski <delwing@gmail.com>
2024-06-23 18:14:52 +0200

    change boolean select into switch

--- src/renderer/src/editor/components/BooleanSelect.tsx
+++ src/renderer/src/editor/components/BooleanSelect.tsx
@@ -1,7 +1,14 @@
 import { JSX } from 'react'
-import { EnumSelect } from './EnumSelect'
 import { InputProperties } from '../Components'
+import { FormCheck } from 'react-bootstrap'
 
-export function BooleanSelect(props: InputProperties): JSX.Element {
-  return EnumSelect([true, false])(props)
+export function BooleanSelect({ name, value, updateCallback }: InputProperties): JSX.Element {
+  return (
+    <FormCheck
+      name={name}
+      type={'switch'}
+      checked={value as boolean}
+      onChange={(e) => updateCallback(e.currentTarget.value === 'true')}
+    />
+  )
 }
